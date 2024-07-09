eabca9c91742f01af0917b09fcb96766fa55198d
Ranjith <34737524+G-Ranjith-Kumar-0319@users.noreply.github.com>
2024-07-09 10:32:47 +0530

    Update chart.tsx

--- chart.tsx
+++ chart.tsx
@@ -1,24 +1,38 @@
 import React, { useState } from "react";
 import LineCharts from "./LineCharts";
 import BarCharts from "./BarCharts";
+import generateColors from "./GenerateColors";
 
-const Charts: React.FC = () => {
+interface ChartsProps {}
+
+const Charts: React.FC<ChartsProps> = () => {
+  const [colorsData, setColorsData] = useState<string[] | undefined>();
   const [highlightedIndex, setHighlightedIndex] = useState<number | null>(null);
 
-  const dataFromBarChart = (data: number | null): void => {
+  const dataFromBarChart = (data: number) => {
     setHighlightedIndex(data);
+  };
+
+  const categoryLength = (length: number) => {
+    setColorsData(generateColors(length));
   };
 
   return (
     <div>
       <h2>Line Chart</h2>
-
       <div style={{ display: "flex" }}>
         <div style={{ width: "50%" }}>
-          <LineCharts highlightedIndex={highlightedIndex} />
+          <LineCharts
+            highlightedIndex={highlightedIndex}
+            categoryLength={categoryLength}
+            colorsData={colorsData}
+          />
         </div>
         <div style={{ width: "50%" }}>
-          <BarCharts dataFromBarChart={dataFromBarChart} />
+          <BarCharts
+            dataFromBarChart={dataFromBarChart}
+            colorsData={colorsData}
+          />
         </div>
       </div>
     </div>
