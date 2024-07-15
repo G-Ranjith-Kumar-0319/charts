c3e905248a7cfe615d89139422f3cc4f31905e3a
Ranjith <34737524+G-Ranjith-Kumar-0319@users.noreply.github.com>
2024-07-15 10:32:13 +0530

    Update chart.tsx

--- chart.tsx
+++ chart.tsx
@@ -1,19 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import LineCharts from "./LineCharts";
 import BarCharts from "./BarCharts";
 import generateColors from "./GenerateColors";
+import LineChartsSecond from "./LineChartsSecond";
+import BarChartsSecond from "./BarChartsSecond";
 
-interface ChartsProps {}
-
-const Charts: React.FC<ChartsProps> = () => {
-  const [colorsData, setColorsData] = useState<string[] | undefined>();
+const Charts: React.FC = () => {
+  const [colorsData, setColorsData] = useState<string[] | undefined>(undefined);
   const [highlightedIndex, setHighlightedIndex] = useState<number | null>(null);
 
-  const dataFromBarChart = (data: number) => {
+  const dataFromBarChart = (data: number | null): void => {
     setHighlightedIndex(data);
   };
 
-  const categoryLength = (length: number) => {
+  const categoryLength = (length: number): void => {
     setColorsData(generateColors(length));
   };
 
@@ -22,16 +22,16 @@ const Charts: React.FC<ChartsProps> = () => {
       <h2>Line Chart</h2>
       <div style={{ display: "flex" }}>
         <div style={{ width: "50%" }}>
-          <LineCharts
+          <LineChartsSecond
             highlightedIndex={highlightedIndex}
             categoryLength={categoryLength}
-            colorsData={colorsData}
+            colorsData={colorsData || []}
           />
         </div>
         <div style={{ width: "50%" }}>
-          <BarCharts
+          <BarChartsSecond
             dataFromBarChart={dataFromBarChart}
-            colorsData={colorsData}
+            colorsData={colorsData || []}
           />
         </div>
       </div>
